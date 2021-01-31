752c67e4ab7a0bb4db08f5aa6d469b8bb70523e8
Yordan <yordansaldanabarrera@gmail.com>
2021-01-31 16:06:51 -0300

    creando Proyecto

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <div>
+            APP
+            <button class="ui primary button">
+                Primary
+                </button>
+            <button class="ui secondary button">
+                Secondary
+                </button>
+        </div>
+    )
+}
+
+export default App;
