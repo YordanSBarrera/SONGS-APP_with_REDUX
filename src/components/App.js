45ac5df6d42a23c4a3bdcfd9a8b83f70f6ac7350
Yordan <yordansaldanabarrera@gmail.com>
2021-03-31 20:20:37 -0300

    sencilla app Uso de REDUX

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,20 @@
-import React from 'react'
-import SongList from './SongList'
-import SongDetail from './SongDetail'
+import React from "react";
+import SongList from "./SongList";
+import SongDetail from "./SongDetail";
 
 const App = () => {
-    return (
-        <div className="ui container grid">
-            <div className="ui row">
-                <div className="column eight wide">
-                    <SongList />
-                </div>
-                <div className="column eight wide">
-                    <SongDetail/>
-                </div>
-            </div>
+  return (
+    <div className="ui container grid" style={{ marginTop: "40px" }}>
+      <div className="ui row">
+        <div className="column eight wide">
+          <SongList />
         </div>
-    )
-}
+        <div className="column eight wide">
+          <SongDetail />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default App;
